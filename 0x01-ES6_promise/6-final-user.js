86650fddcc90d6d6e32478829db79182f8ccf2cc
deenakamal <deenah.kamal@gmail.com>
2024-06-27 00:00:35 +0000

    6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => 
+      results.map((result) => {
+        if (result.status === 'rejected') {
+          return {
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          };
+        }
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      })
+    )
+    .catch(() => console.log('Signup system offline'));
+}
