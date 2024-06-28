8cbf62413976a7d4589d432d7ced6dfb4522ca9b
deenakamal <deenah.kamal@gmail.com>
2024-06-28 02:24:12 +0000

    edit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,4 +2,5 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const numbers = [19, 20, 34];
-  return numbers.map((size) => new ClassRoom(size))
+  return numbers.map((size) => new ClassRoom(size));
+}
