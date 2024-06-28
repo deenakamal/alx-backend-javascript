646b92920529dbe4f7e58cf4b7ba8cc1924dd74f
deenakamal <deenah.kamal@gmail.com>
2024-06-28 03:12:33 +0000

    task 8

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be string');
+    }
+
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be string');
+    }
+
+    this._code = newCode;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
